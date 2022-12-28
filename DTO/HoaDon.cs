fede71419ccd1e9446e8f7483dc4b3bf9d72c2ae
namnh <namnh@dolphinsolutions.vn>
2022-12-28 13:21:46 +0700

    Add project files.

---
+++ DTO/HoaDon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HoaDon
+    {
+        public string maHD { get; set; }
+        public string maNV { get; set; }
+        public string maKH { get; set; }
+        public DateTime ngayHD { get; set; }
+        public float tongTien { get; set; }
+    }
+}
